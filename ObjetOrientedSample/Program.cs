69b1c70e74b7d96704b6664b0b145c75be82c27a
Süleyman UZUNKÖPRÜ <s-uzunkopru@yandex.com>
2022-11-19 21:52:18 +0300

    : base(boy, kilo)

--- ObjetOrientedSample/Program.cs
+++ ObjetOrientedSample/Program.cs
@@ -5,11 +5,11 @@ public interface IKisi
 {
     public string Ad { get; set; }
     public string Soyad { get; set; }
-    public DateTime DogumTarihi { get; set; }
-    public int Yas(DateTime DogTar);
+    public DateTime DogTar { get; set; }
+    public int Yas(DateTime dogTar);
     public float VucutKitleEndeksi(float boy, float kilo);
 }
-public interface IPersonel : IKisi
+public interface IPersonel
 {
     public DateTime IseGirisTarihi { get; set; }
     public long SGK_No { get; set; }
@@ -18,20 +18,22 @@ public abstract class Kisi : IKisi
 {
     public string Ad { get; set; }
     public string Soyad { get; set; }
-    public DateTime DogumTarihi { get; set; }
-    private float Boy;
-    private float Kilo;
-    private DateTime DogTar;
-    protected Kisi(string ad, string soyad, DateTime dogTar)
-                        => (Ad, Soyad, DogTar) = (ad, soyad, dogTar);
+    public DateTime DogTar { get; set; }
+    public float Boy;
+    public float Kilo;
+    public Kisi(string ad, string soyad, DateTime dogTar)
+                                    => (Ad, Soyad, DogTar)
+                                    = (ad, soyad, dogTar);
+    public Kisi(float boy, float kilo) 
+                                => (Boy, Kilo) = (boy, kilo);
     protected Kisi() { }
     public abstract int Yas(DateTime DogTar);
     public float VucutKitleEndeksi(float boy, float kilo)
-                                                    => kilo / (boy * boy);
+                                                => kilo / (boy * boy);
     public decimal VucutKitleEndeksi(decimal boy, decimal kilo)
-                                                    => kilo / (boy * boy);
+                                                 => kilo / (boy * boy);
     public double VucutKitleEndeksi(double boy, double kilo)
-                                                    => kilo / (boy * boy);
+                                                 => kilo / (boy * boy);
 }
 public class Personel : Kisi, IPersonel
 {
@@ -40,32 +42,32 @@ public class Personel : Kisi, IPersonel
     public override int Yas(DateTime DogTar)
                                     => Today.Year - DogTar.Year;
     public Personel(string ad, string soyad, DateTime dogTar,
-                            DateTime iseGiris, long sgkNo)
-                            : base(ad, soyad, dogTar)
-                            => (IseGirisTarihi, SGK_No) = (iseGiris, sgkNo);
-    public Personel() { }
+                               DateTime iseGiris, long sgkNo)
+                               : base(ad, soyad, dogTar)
+                               => (IseGirisTarihi, SGK_No)
+                               = (iseGiris, sgkNo);
+    public Personel(float boy, float kilo)
+                                : base(boy, kilo)
+                                => (Boy, Kilo) = (boy, kilo);
 }
 class Program
 {
     static void Main(string[] args)
     {
-        Personel personel = new Personel()
-        {
-            Ad = "Süleyman",
-            Soyad = "UZUNKÖPRÜ",
-            DogumTarihi = new DateTime(1976, 11, 17),
-            IseGirisTarihi = new DateTime(1995, 05, 01),
-            SGK_No = 3403199606293,
-        };
-
+        Personel personel = new Personel
+           (ad: "Süleyman",
+            soyad: "UZUNKÖPRÜ",
+            dogTar: new DateTime(1976, 11, 17),
+            iseGiris: new DateTime(1995, 05, 01),
+            sgkNo: 3403199606293);
         WriteLine($"Ad: {personel.Ad}");
         WriteLine($"Soyad: {personel.Soyad}");
         Write("Doğum Tarihi: ");
-        WriteLine(personel.DogumTarihi.ToShortDateString());
+        WriteLine(personel.DogTar.ToShortDateString());
         Write("İse Giriş Tarihi: ");
         WriteLine(personel.IseGirisTarihi.ToShortDateString());
         WriteLine($"SGK No: {personel.SGK_No}");
-        WriteLine($"Yaşı: {personel.Yas(personel.DogumTarihi)}");
+        WriteLine($"Yaşı: {personel.Yas(personel.DogTar)}");
         Write($"{nameof(personel.VucutKitleEndeksi)}");
         Write($"{typeof(float)}:");
         WriteLine($"{personel.VucutKitleEndeksi(1.78f, 99.5f)}");
