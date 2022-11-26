b9ad24605c4fd68b1257ad87dba71eeb859df94b
Süleyman UZUNKÖPRÜ <s-uzunkopru@yandex.com>
2022-11-26 13:04:20 +0300

    MethodsParametersVoidReturn

---
+++ DynamicSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+using System.Drawing;
+static int AracYasTip(dynamic arac, out Type type, int model)
+{
+    type = arac.GetType();
+    arac.Model = model;
+    return arac.Yas;
+}
+Type tipiNE;
+Araba araba = new();
+WriteLine($"{AracYasTip(araba, out tipiNE, 2016)} yaşında {tipiNE}");
+Motor motor = new();
+WriteLine($"{AracYasTip(motor, out tipiNE, 2014)} yaşında {tipiNE}");
+ReadLine();
+public enum Vites
+{
+    Manuel, Otomatik
+}
+public class Araba
+{
+    public Color Renk { get; set; }
+    public Vites Vites { get; set; }
+    public int Model { get; set; }
+    public int Yas => DateTime.Today.Year - Model;
+}
+class Motor : Araba
+{
+    public bool TopCase { get; set; }
+}
---
+++ SimplifiedOrderingWith/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+int[] yillar = { 2002, 2005, 1997, 2022, 2013 };
+//var sirali = yillar.OrderBy(x => x); //Net 6
+var sirali = yillar.Order(); //Net 7
+WriteLine("Sıralama Yok");
+foreach (var str in yillar) WriteLine(str);
+WriteLine("Sıralama Simplified Ordering With .Net 7");
+foreach (var str in sirali) WriteLine(str);
+ReadLine();
