19075cab22940f1ab1bae3d907bf96f1dac115bd
Süleyman UZUNKÖPRÜ <s-uzunkopru@yandex.com>
2022-11-26 18:38:04 +0300

    refParameterModifier

---
+++ MethodsOutModifier/Program.cs
@@ -0,0 +1,27 @@
+﻿double ort, mak, min, adt;
+List<double> sayilarim = new();
+double[] sayilar =
+    { 499.75, 1_999.85, 1_501.15,
+            999.25, 5_250, 4_750, 15_000 };
+sayilarim.AddRange(sayilar);
+double toplam = Topla(sayilarim,
+    out ort, out mak, out min, out adt);
+Console.WriteLine($"Toplam: {toplam:0,0.00}");
+Console.WriteLine($"Ortalama: {ort:0,0.00}");
+Console.WriteLine($"Maksimum: {mak:0,0.00}");
+Console.WriteLine($"Minimum: {min:0,0.00}");
+Console.WriteLine($"Adet: {adt}");
+Console.ReadKey();
+static double Topla
+    (List<double> sayilar, out double ortalama,
+        out double maks, out double min, out double adet)
+{
+    ortalama = Math.Round(sayilar.Average(), 2);
+    maks = Math.Round(sayilar.Max(), 2);
+    min = Math.Round(sayilar.Min(), 2);
+    adet = sayilar.Count;
+    return Math.Round(sayilar.Sum(), 2);
+}
+
+
+
---
+++ MethodsParametersVoidReturn/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Globalization;
+
+namespace MethodsParametersVoidReturn;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string ayracNedir = SistemNumerikAyraci();
+        AyracOku(ayracNedir); 
+        ayracNedir = SistemNumerikAyraci(1055);
+        AyracOku(ayracNedir); 
+        bool onlukBinlik = false;
+        ayracNedir = SistemNumerikAyraci(onlukBinlik);
+        AyracOku(onlukBinlik, ayracNedir);
+        Console.ReadKey();
+    }
+    /// <summary>
+    /// Sistemdeki sayıların ondalık kısımları için kullanılan 
+    /// ayracı döndürür.
+    /// </summary>
+    /// <returns>Dönüş değeri string tipindedir. 
+    /// Nokta veya virgül döner</returns>
+    static string SistemNumerikAyraci()
+    {
+        return CultureInfo.CurrentCulture.NumberFormat
+                                .NumberDecimalSeparator;
+    }
+    /// <summary>
+    /// Sistemdeki sayıların ondalık kısımları için kullanılan 
+    /// ayracı döndürür.
+    /// </summary>
+    /// <param name="Kod">Parametre olarak, ondalık ayraç
+    /// istenen ülke kodunu alır.</param>
+    /// <returns>Dönüş değeri string tipindedir. 
+    /// Nokta veya virgül döner</returns>
+    static string SistemNumerikAyraci(int Kod=1055)
+    {
+        CultureInfo cultureInfo = new(Kod);
+        return cultureInfo.NumberFormat.NumberDecimalSeparator;
+    }
+
+    /// <summary>
+    /// Sistemdeki sayıların ondalık veya binlik 
+    /// kısımları için kullanılan
+    /// ayracı döndürür.
+    /// </summary>
+    /// <param name="On_Bin">Ondalık mı, binlik ayraç istediğinizi 
+    /// sorar. 
+    /// Eğer <code>true</code> gönderirseniz Ondalık Ayraç döner. 
+    /// Eğer <code>false</code> gönderirseniz Binlik Ayraç döner. 
+    /// </param>
+    /// <remarks>
+    /// Ayraçların doğru olarak bulunması sayesinde, sayıların
+    /// okunması sırasında bir hata ile karşılaşmayacaksınız.
+    /// </remarks>
+    /// <returns>Dönüş değeri string tipindedir. 
+    /// Nokta veya virgül döner</returns>
+    static string SistemNumerikAyraci(bool On_Bin = true)
+    {
+        CultureInfo tr = new(1055);
+        NumberFormatInfo f = tr.NumberFormat;
+        string ayrac = On_Bin ? f.NumberDecimalSeparator
+                              : f.NumberGroupSeparator;
+        return ayrac;
+    }
+    /// <summary>
+    /// Bu metot ondalık ayracı okur.
+    /// </summary>
+    static void AyracOku()
+    {
+        string oku = "Ondalık Ayraç : ";
+        oku += CultureInfo.CurrentCulture.NumberFormat
+            .NumberDecimalSeparator == "," 
+                    ? "Virgül (,)" : "Nokta (.)";
+        Console.WriteLine(oku);
+    }
+    static void AyracOku(string OndalikAyrac)
+    {
+        string oku = "Ondalık Ayraç : ";
+        oku += OndalikAyrac == "," 
+                    ? "Virgül (,)" : "Nokta (.)";
+        Console.WriteLine(oku);
+    }
+    static void AyracOku(bool AyracTipi, string Ayrac)
+    {
+        string oku = AyracTipi ? "Ondalık Ayraç : "
+                               : "Binlik Ayraç : ";
+        oku += Ayrac == "," ? "Virgül (,)" : "Nokta (.)";
+        Console.WriteLine(oku);
+    }
+}
