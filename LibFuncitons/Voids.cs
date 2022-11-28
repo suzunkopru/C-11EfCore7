2ce41694e26758abd175a32f76bbe00594e7067c
Süleyman UZUNKÖPRÜ <s-uzunkopru@yandex.com>
2022-11-28 21:17:51 +0300

    Voids.Ciz

---
+++ LibFuncitons/Voids.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+using static System.Linq.Enumerable;
+
+namespace LibFuncitons;
+public static class Voids
+{
+    public static void Ciz(char ne, int kac)
+        => WriteLine(string.Concat(Repeat(ne, kac)));
+}
---
+++ MemberAccessOperator/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+string[] ulkeler = new string[5];
+ulkeler[0] = "Türkiye";
+ulkeler[1] = "Libya";
+ulkeler[2] = "Azerbaycan";
+ulkeler[3] = "Bulgaristan";
+ulkeler[4] = "Amerika";
+WriteLine($"4.elemanı: {ulkeler[4]}");
+string[] iller =
+    { "İstanbul", "Ankara", "İzmir", "Bursa", "Rize", "Malatya" };
+WriteLine($"0.elemanı: {iller[0]}, 1.elemanı: {iller[1]}, ");
+//C# 8.0 Index From End
+WriteLine($"iller dizisinin sondan ikinci elemanı: {iller[^2]}");
+//C# 8.0 Ranges
+WriteLine($"iller dizisinden [..3] eleman:");
+WriteLine(string.Join(Environment.NewLine, iller[..3]));
+WriteLine($"iller dizisinden [3..] eleman:");
+WriteLine(string.Join(Environment.NewLine, iller[3..]));
+WriteLine($"iller dizisinden [..] eleman:");
+WriteLine(string.Join(Environment.NewLine, iller[..]));
+ReadLine();
