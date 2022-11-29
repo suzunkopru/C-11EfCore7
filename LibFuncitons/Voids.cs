437e0b7c3b290d2496f4376710fb833f11bd1e53
Süleyman UZUNKÖPRÜ <s-uzunkopru@yandex.com>
2022-11-29 22:14:35 +0300

    NullConditionalSample

--- LibFuncitons/Voids.cs
+++ LibFuncitons/Voids.cs
@@ -6,4 +6,6 @@ public static class Voids
 {
     public static void Ciz(char ne, int kac)
         => WriteLine(string.Concat(Repeat(ne, kac)));
+    public static void JoinList(int[] ints) 
+        => WriteLine(string.Join("\n", ints));
 }
---
+++ NullConditionalSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using static LibFuncitons.Voids;
+
+int? integer = null;
+WriteLine(integer ?? int.MaxValue);
+List<int[]>? integrals = new List<int[]>
+     {
+        new [] { 200, 320, 300, 100, 800 },
+        new [] { 600, 250, 325, 115, 875 },
+        null
+    };
+WriteLine($"integrals 0. Satır 0. Sütun: {integrals[0][0]}");
+WriteLine($"integrals 0. Satır 1. Sütun: {integrals[0][1]}");
+WriteLine($"integrals 1. Satır 2. Sütun: {integrals[1][2]}");
+WriteLine($"integrals 1. Satır 3. Sütun: {integrals[1][3]}");
+Ciz('-', 32);
+JoinList(integrals[0]);
+Ciz('-', 32);
+WriteLine($"Toplama metodu: {Toplama(integrals, 0)}");
+WriteLine($"Toplama metodu: {Toplama(integrals, 2)}");
+int[] intsnewInts = { 55, 15, 11, 30 };
+JoinList(intsnewInts);
+Ciz('+', 5);
+WriteLine($"{Topla(intsnewInts)}");
+WriteLine(intsnewInts.Where(x => x > 20).Sum());
+Ciz('-', 32);
+ReadKey();
+static int? Topla(int[]? sayilar) => sayilar?.Sum();
+static int Toplama(List<int[]>? ints, int start)
+                        => ints?[start]?.Sum() ?? 9999;
