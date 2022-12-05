3d0b7949c3f7f6f917a6b2ce3936cc624e735d9e
Süleyman UZUNKÖPRÜ <s-uzunkopru@yandex.com>
2022-12-05 19:00:32 +0300

    AllWindowsForm

---
+++ BreakContinue/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+gotoDongusu:
+Write("4 Dönem mevcuttur. Buna göre bir dönem değeri yazınız.");
+WriteLine("Ekranda Kaç Dönem Yazdırılacak? Bir Sayı giriniz.");
+bool veriGir = int.TryParse(ReadLine(), out int dnm);
+if (veriGir == false)
+{
+    WriteLine("Bir sayı girmeniz gerekmez mi?");
+    goto gotoDongusu;
+}
+dnm = dnm > 4 ? 4 : dnm <= 0 ? 1 : dnm;
+int donemSayaci = 0;
+int yil = DateTime.Today.Year;
+for (int i = 1; i <= 12; i++)
+{
+    int mod = i % 3;
+    if (mod != 0) continue;
+    if (donemSayaci == dnm) break;
+    donemSayaci++;
+    string ayAdi = new DateOnly(yil, i, 1).ToString("MMMM");
+    Write($"{yil} Yılı {i}. Ay ({ayAdi}) ayının son günü: ");
+    WriteLine($"{DateTime.DaysInMonth(yil, i)}");
+}
+ReadKey();
---
+++ returnStatement/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+static nint Kuvvet(int sayi, int kuvvet)
+{
+    nint hesapla = 1;
+    for (int i = 0; i < kuvvet; i++)
+    {
+        hesapla *= sayi;
+    }
+    return hesapla;
+}
+WriteLine($"Kuvvet(2, 32): {Kuvvet(2, 32):0,0}");
+WriteLine($"Math.Pow(2, 32): {Math.Pow(2, 32):0,0}");
+Console.ReadKey();
